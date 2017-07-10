1a5eb38bd11058d1ea2a853921f818c5fb177fde
bfoley-teamug <withtechron@gmail.com>
2017-07-10 20:13:59 +0000

    validate password in jquery

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ var $confirm = $("#confirm_password");
 
 $("form span").hide();
 
+function validPW() {
+ return $password.val().length > 8; 
+}
+
+function matchingPW() {
+ return $password.val() === $confirm.val(); 
+}
+
+function canSubmit() {
+ return validPW() && matchingPW(); 
+}
+
